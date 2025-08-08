bd7807011ab5a8c357800af6bf9b8c1f04480e2a
Ben Brobst <brobst464@gmail.com>
2025-08-07 20:26:46 -0400

    Finished program :)

--- main.cpp
+++ main.cpp
@@ -1,3 +1,24 @@
-int main() {
+#include <iostream>
+#include "Book.h"
 
+
+int main() {
+	Book book1("Harry Potter", "Jerry R. Sprousling", "82983-3892-3893", true);
+
+	if (book1.checkout()) {
+		std::cout << "Book checked out successfully.\n";
+	}
+	else {
+		std::cout << "Book is already checked out.\n";
+	}
+	book1.displayInfo();
+	std::cout << "\n";
+
+	if (book1.returnBook()) {
+		std::cout << "Book returned successfully.\n";
+	}
+	else {
+		std::cout << "Book is already available.\n";
+	}
+	book1.displayInfo();
 }
